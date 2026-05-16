4f352a7dfe12e66f2e3d9ac4918ccb167af14d38
umserhumanoeuacho <mimiguel0912@gmail.com>
2026-05-16 20:14:09 -0300

    Add ColorsCommand to display available colors
    Implement ColorsCommand to list available ChatColors.

--- src/main/java/me/miguel/title/commands/ColorsCommand.java
+++ src/main/java/me/miguel/title/commands/ColorsCommand.java
@@ -1,1 +1,24 @@
+package me.miguel.title.commands;
 
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class ColorsCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        sender.sendMessage("§6===== CORES DISPONÍVEIS =====");
+
+        for (ChatColor color : ChatColor.values()) {
+
+            if (color.isColor()) {
+                sender.sendMessage(color + color.name());
+            }
+        }
+
+        return true;
+    }
+}

30f706b499d91098b8e53b827ea6c9ff6da32ae1
umserhumanoeuacho <mimiguel0912@gmail.com>
2026-05-16 20:14:54 -0300

    Update UnsetInitialTitleCommand.java

--- src/main/java/me/miguel/title/commands/UnsetInitialTitleCommand.java
+++ src/main/java/me/miguel/title/commands/UnsetInitialTitleCommand.java
@@ -1,1 +1,26 @@
+package me.miguel.title.commands;
 
+import me.miguel.title.Main;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class UnsetInitialTitleCommand implements CommandExecutor {
+
+    private final Main plugin;
+
+    public UnsetInitialTitleCommand(Main plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        plugin.getConfig().set("initial", null);
+        plugin.saveConfig();
+
+        sender.sendMessage("§cTítulo inicial removido!");
+
+        return true;
+    }
+}
